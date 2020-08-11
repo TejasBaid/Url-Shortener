b69f1917e5df8a96807d08973edb8d96b52756a2
tejasbaid <tejasbaid3@gmail.com>
2020-08-11 17:12:55 +0530

    Updates

---
+++ precache-manifest.4435509a45c4dce92aa7ed4472327327.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba31819ac5a1f06e4c243fe8f690ab20",
+    "url": "/Url-Shortener/index.html"
+  },
+  {
+    "revision": "6b8cb59294b739b34fe6",
+    "url": "/Url-Shortener/static/css/main.87251ec5.chunk.css"
+  },
+  {
+    "revision": "8c2db61605d9046e8d91",
+    "url": "/Url-Shortener/static/js/2.97a8c5f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Url-Shortener/static/js/2.97a8c5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8cb59294b739b34fe6",
+    "url": "/Url-Shortener/static/js/main.bb0aeb8f.chunk.js"
+  },
+  {
+    "revision": "b9d2fe9a7389b58ba1a9",
+    "url": "/Url-Shortener/static/js/runtime-main.7d9d884a.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/Url-Shortener/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/Url-Shortener/static/media/desktop-input.a82de60e.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/Url-Shortener/static/media/homepage.5029d049.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/Url-Shortener/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "996b697c33ab8772e59e3c9bec1124ea",
+    "url": "/Url-Shortener/static/media/icon-instagram.996b697c.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/Url-Shortener/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/Url-Shortener/static/media/icon-twitter.cb4fea0b.svg"
+  }
+]);
